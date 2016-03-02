79ad0447bb581141fecc9fff9dd1cf5b5330313c
並木一晃 <kazu0919frontale@i.softbank.jp>
2016-03-02 20:54:35 +0900

    Imageを追加しました。

--- Minwari/AppDelegate.swift
+++ Minwari/AppDelegate.swift
@@ -10,48 +10,38 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UITextFieldDelegate {
-
+    
     var window: UIWindow?
-
-
-    let storyboard: UIStoryboard = self.grabStoryboard()
-    if let window = window {
-        window.rootViewController = storyboard.instantiateInitialViewController(); UIViewController(); UIViewController();  UIViewController()
+    
+    func grabStoryboard() -> UIStoryboard {
+        var storyboard = UIStoryboard()
+        let height = UIScreen.mainScreen().bounds.size.height
+        
+        if height == 480 {
+            storyboard = UIStoryboard(name: "3.5inch", bundle: nil)
+        }else if height == 667 {
+            storyboard = UIStoryboard(name: "4.7inch", bundle: nil)
+        }else if height == 736 {
+            storyboard = UIStoryboard(name: "5.5inch", bundle: nil)
+        } else {
+            storyboard = UIStoryboard(name: "4inch", bundle: nil)
+        }
+        return storyboard
     }
     
-    self.window?.makeKeyAndVisible()
-    
-    return true
-}
-
-func grabStoryboard() -> UIStoryboard {
-    var storyboard = UIStoryboard()
-    let height = UIScreen.mainScreen().bounds.size.height
-    
-    //iPhone6 Plus
-    if height == 736 {
-        storyboard = UIStoryboard(name: "5.5inchStoryboard", bundle: nil)
-        //iPhone6
-    }else if height == 667 {
-        storyboard = UIStoryboard(name: "Main", bundle: nil)
-        //iPhone4/4s
-    }else if height == 480 {
-        storyboard = UIStoryboard(name: "3.5inchStoryboard", bundle: nil)
-        //iPad
-    }else if height == 1024 {
-        storyboard = UIStoryboard(name: "iPad", bundle: nil)
-        //iPhone5・5s・5c
-    }else {
-        storyboard = UIStoryboard(name: "4inchStoryboard", bundle: nil)
-    }
-    return storyboard
-}
-
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let storyboard: UIStoryboard = self.grabStoryboard()
+            if let window = window {
+            window.rootViewController = storyboard.instantiateInitialViewController() as UIViewController!
+        }
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
